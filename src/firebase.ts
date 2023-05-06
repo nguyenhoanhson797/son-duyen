4d3eb24fb35e6e66cb2e6f5d210ca6eb07f20eb9
SonHoanh <nguyenhoanhson797@gmail.com>
2023-05-06 12:44:10 +0700

    init

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import firebase, { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUJUhAZrlepUr8724lQYqcF5HMAK_z-Xw",
+  authDomain: "wedding-invitation-98d6a.firebaseapp.com",
+  projectId: "wedding-invitation-98d6a",
+  storageBucket: "wedding-invitation-98d6a.appspot.com",
+  messagingSenderId: "90962351036",
+  appId: "1:90962351036:web:f21fc4bf5688fdbf58d1b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
